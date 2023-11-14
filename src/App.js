f1c4a40759ed4f7d0a498d8dcbbfed7fdb947470
dave cen <davecen9@gmail.com>
2023-11-14 17:36:22 -0500

    save

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import SearchBar from "./components/SearchBar";
+
 function App() {
+
+    const handleSubmit = (term) => {
+        console.log("Do a search with", term);
+    };
+
+
     return (
-    <div> Testing </div>
+    <div> <SearchBar onSubmit={handleSubmit}/> </div>
     )
 }
 
--- src/api.js
+++ src/api.js
@@ -1,18 +1,18 @@
 import axios from "axios";
 
-const searchImages = async () => {
+const searchImages = async (term) => {
     const response = await axios.get("https://api.unsplash.com/search/photos",
     {
         headers :{
             Authorization : "Client-ID 7no41hhmXq1SXhuVERZ5PefjMx00rbsrLmQCqxQox_g"
         },
         params:{
-            query: "cars"
+            query: "car"
         }
     });
 
     console.log(response);
-    return response;
+    return response.data.results;
 };
 
 export default searchImages;
